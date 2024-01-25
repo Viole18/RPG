14bb8b6f5a08d66b5e1a12e0b379dbe1ba0dd977
Viole18 <violejgrays@gmail.com>
2024-01-25 17:25:14 +0300

    add com

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ const locations = [
     text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
   }
 ];
-
+// kaif
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
